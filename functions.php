f50794eacd01a4a20cb8507368e74370d5b27d08
Mohammad Afzali <81291770+afzaliwp@users.noreply.github.com>
2025-02-22 20:47:17 +0330

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,7 +4,32 @@ function afzaliwp_boilerplate_autoload( $class_name ) {
 		return;
 	}
 
-	$file = str_replace( [ '_', 'Afzaliwp\Boiler_Plate', '\\' ], [ '-', __DIR__, '/' ], $class_name ) . '.php';
+		$file = str_replace(
+		        [
+			        '_',
+			        strtolower( 'Afzaliwp\Boiler-Plate' ),
+			        '\\',
+		        ],
+		        [
+			        '-',
+			        __DIR__,
+			        DIRECTORY_SEPARATOR,
+		        ],
+		        strtolower( $class_name ) ) . '.php';
 
-	require_once strtolower( $file );
+	require_once $file;
+}
+
+if ( ! function_exists( 'mylog' ) ) {
+	function mylog( $data, $other_data = '' ) {
+		error_log( PHP_EOL . '-----------------------------------' );
+		error_log( '-------------$data: .' . $other_data . '---------------' );
+		if ( is_array( $data ) || is_object( $data ) ) {
+			error_log( print_r( $data, true ) );
+		} else {
+			error_log( $data );
+		}
+		error_log( '-----------------------------------' );
+		error_log( '-----------------------------------' . PHP_EOL );
+	}
 }
